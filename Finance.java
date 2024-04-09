82dc2edf1ad3162f1202fbfe4cb5ed0c65738a23
husaab <126218851+husaab@users.noreply.github.com>
2024-04-09 20:15:36 +0000

    Co-authored-by: Daniel P <DmPin@users.noreply.github.com> Co-authored-by: Dev Patel <DP726@users.noreply.github.com>
    added sign up interface code

--- Finance.java
+++ Finance.java
@@ -1,3 +1,5 @@
 public class Finance {
-    
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+    }
 }
--- Settings.java
+++ Settings.java
@@ -1,3 +1,5 @@
 public class Settings {
-    
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+    }
 }
