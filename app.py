42d6d3a52cf968ae1608b8e5c7ae9b05b7164e8f
husaab <husseinsaabsrs@gmail.com>
2024-04-09 16:43:15 -0400

    Co-authored-by: Daniel P <DmPin@users.noreply.github.com> Co-authored-by: Josephhenok <Josephhenok@users.noreply.github.com> Co-authored-by: Dev Patel <DP726@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('home.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            flash('You were successfully logged in')
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
