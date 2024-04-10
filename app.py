6060db46c23d468b602009069e93ae8ce1e4d8dc
DP726 <134741701+DP726@users.noreply.github.com>
2024-04-10 15:37:46 -0400

    made database

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+# Models
+class Profile(db.Model):
+    # Id : Field which stores unique id for every row in 
+    # database table.
+    # first_name: Used to store the first name if the user
+    # last_name: Used to store last name of the user
+    # Age: Used to store the age of the user
+    username = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(20), unique=False, nullable=False)
+    last_name = db.Column(db.String(20), unique=False, nullable=False)
+    email = db.Column(db.String(30), nullable=False)
+ 
+    # repr method represents how one object of this datatable
+    # will look like
+    def __repr__(self):
+        return f"Username: {self.username} Name : {self.first_name, self.last_name}, Email: {self.email}"
 
 @app.route('/')
 def hello_world():
