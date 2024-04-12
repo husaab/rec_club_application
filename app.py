89e92a9034299c9a4b2edc7d0be3c7f814ecef8a
DP726 <134741701+DP726@users.noreply.github.com>
2024-04-12 14:42:42 -0400

    database fully works

--- app.py
+++ app.py
@@ -3,35 +3,44 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SECRET_KEY'] = 'abcdefghijklmnopqrstuvwxyz'
 
 db = SQLAlchemy(app)
 
 # Models
-class Profile(db.Model):
-    # Id : Field which stores unique id for every row in 
-    # database table.
-    # first_name: Used to store the first name if the user
-    # last_name: Used to store last name of the user
-    # Age: Used to store the age of the user
-    username = db.Column(db.Integer, primary_key=True)
+class Profiles(db.Model):
+
+    username = db.Column(db.String(20), primary_key=True)
     first_name = db.Column(db.String(20), unique=False, nullable=False)
     last_name = db.Column(db.String(20), unique=False, nullable=False)
+    password = db.Column(db.String(100), unique=False, nullable=False)
     email = db.Column(db.String(30), nullable=False)
  
     # repr method represents how one object of this datatable
     # will look like
-    def __repr__(self):
-        return f"Username: {self.username} Name : {self.first_name, self.last_name}, Email: {self.email}"
+    #def __repr__(self):
+    #    return f"Username: {self.username} Name : {self.first_name, self.last_name}, Email: {self.email}"
+
+    def __init__(self, username, first_name, last_name, password, email):
+        self.username = username
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
+        self.email = email
+
+    def check_password(self, password):
+        return self.password == password
 
 @app.route('/')
-def hello_world():
-    return render_template('home.html')
+def home():
+    return render_template('home.html', Profiles = Profiles.query.all() )
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+        username = Profiles.query.filter_by(username=request.form['username']).first()
+        if username is None or not username.check_password(request.form['password']):
             error = 'Invalid Credentials. Please try again.'
         else:
             flash('You were successfully logged in')
@@ -41,14 +50,26 @@ def login():
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
+        if not request.form['new_username'] or not request.form['new_password'] or not request.form['confirm_password'] or not request.form['email']:
+            flash("Please fill out all the required fields", "error")
+        else:
+            existing_profile = Profiles.query.filter_by(username=request.form['new_username']).first()
+            if existing_profile:
+                flash("User already exists.")
+            else:
+                profile = Profiles(request.form['new_username'], request.form['first_name'], request.form['last_name'], request.form['new_password'], request.form['email'])
+                db.session.add(profile)
+                db.session.commit()
         # Here, insert code to handle the sign-up form submission.
         # This could involve validating the form data, checking if the
         # username is already taken, and storing the new user's information
         # in the database.
-        return redirect(url_for('login'))
+                return redirect(url_for('login'))
     return render_template('signup.html')
 
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
     
